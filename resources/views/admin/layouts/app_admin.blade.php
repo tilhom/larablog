b2bfcdb682da4416dcd1979a7848b6576bcdff6b
Ilya Vech <1qadam@mail.ru>
2018-02-13 12:36:20 +0500

    Create category.index page

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
@@ -41,7 +42,7 @@
                         Блог
                         </a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Категории</a></li>
+                            <li><a href="{{route('admin.category.index')}}">Категории</a></li>
                             <li><a href="#">Материалы</a></li>
                         </ul>
                         <li>
